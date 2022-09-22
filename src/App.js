c95aa5e7f3596dac42cc5a14a3b35b5f973acb7d
jesukoseemani <olowosusiayo@gmail.com>
2022-09-22 10:52:58 +0100

    i added the drowdown menu

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import CoreProduct from './components/CoreProduct';
@@ -5,12 +6,15 @@ import PaymentSection from './components/PaymentSection';
 import FinanceSection from './components/FinanceSection';
 import Sponsor from './components/Sponsor';
 import Footer from './components/Footer';
+import Overlay0ne from './components/Overlay0ne';
 
 function App() {
+	const [isLabel, setIsLabel] = useState('');
+
 	return (
-		<div className='w-full max-w-[1728px] m-auto scrollbar-hide'>
+		<div className='w-full max-w-[1728px] m-auto scrollbar-hide relative'>
 			<div className='bg-[#F4FDF8] scrollbar-hide'>
-				<Navbar />
+				<Navbar isLabel={isLabel} setIsLabel={setIsLabel} />
 				<Header />
 			</div>
 			<CoreProduct />
@@ -18,6 +22,16 @@ function App() {
 			<FinanceSection />
 			<Sponsor />
 			<Footer />
+
+			{isLabel !== '' ? (
+				<div
+					onClick={() => setIsLabel('')}
+					className='absolute top-[72px] left-0 bg-green-600/30 backdrop-blur-sm w-full h-[100%] z-[1000]'>
+					<Overlay0ne />
+				</div>
+			) : (
+				''
+			)}
 		</div>
 	);
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,16 @@ import logo from '../assets/icons/logo.svg';
 import hamburger from '../assets/icons/icon-hamburger.svg';
 import close from '../assets/icons/icon-close.svg';
 
-const Navbar = () => {
+const Navbar = ({ isLabel, setIsLabel }) => {
 	const [navDisplay, setNavDisplay] = useState(false);
-
+	const isLabelHandler = (identifier) => {
+		setIsLabel(identifier);
+	};
 	return (
-		<div className='w-full h-[72px] flex justify-between items-center px-[120px] lg:px-[10px] bg-[#F4FDF8]'>
+		<div
+			className={`w-full h-[72px] flex justify-between items-center px-[120px] lg:px-[10px] ${
+				isLabel ? 'bg-green-600 sticky top-0 left-0 z-[2000]' : 'bg-[#F4FDF8]'
+			} `}>
 			<div>
 				<img
 					className='select-none hidden md:block'
@@ -22,20 +27,34 @@ const Navbar = () => {
 			</div>
 			<div className={`flex  ${navDisplay ? 'space-x-0' : 'space-x-[64px]'}`}>
 				<div className='flex items-center space-x-[32px] md:hidden'>
-					<li className='flex items-center text-[16px] text-[#0A0903] font-medium'>
+					<li
+						onClick={() => isLabelHandler('products')}
+						className='flex items-center text-[16px] text-[#0A0903] font-medium cursor-pointer relative before:w-0 before:h-1 before:absolute before:top-[-15px] before:right-0 before:bg-green-600 before:transition-all before:duration-500 hover:before:w-full hover:before:left-0 hover:before:bg-green-600'>
 						Products{' '}
-						<span className='ml-[15px]'>
+						<span
+							className={`ml-[15px] ${
+								isLabel === 'products' && 'transition-all rotate-180'
+							}`}>
 							<img src={Keydown} alt='' />
 						</span>{' '}
 					</li>
-					<li className='flex items-center text-[16px] text-[#0A0903] font-medium'>
+					<li
+						onClick={() => isLabelHandler('business')}
+						className='flex items-center text-[16px] text-[#0A0903] font-medium cursor-pointer relative before:w-0 before:h-1 before:absolute before:top-[-15px] before:right-0 before:bg-green-600 before:transition-all before:duration-500 hover:before:w-full hover:before:left-0 hover:before:bg-green-600'>
 						Businesses{' '}
-						<span className='ml-[15px]'>
+						<span
+							className={`ml-[15px] ${
+								isLabel === 'business' && 'transition-all rotate-180'
+							}`}>
 							<img src={Keydown} alt='' />
 						</span>
 					</li>
-					<li className='text-[16px] text-[#0A0903] font-medium'>About</li>
-					<li className='text-[16px] text-[#0A0903] font-medium'>Careers</li>
+					<li className='text-[16px] text-[#0A0903] font-medium cursor-pointer relative before:w-0 before:h-1 before:absolute before:top-[-15px] before:right-0 before:bg-green-600 before:transition-all before:duration-500 hover:before:w-full hover:before:left-0 hover:before:bg-green-600'>
+						About
+					</li>
+					<li className='text-[16px] text-[#0A0903] font-medium cursor-pointer relative before:w-0 before:h-1 before:absolute before:top-[-15px] before:right-0 before:bg-green-600 before:transition-all before:duration-500 hover:before:w-full hover:before:left-0 hover:before:bg-green-600'>
+						Careers
+					</li>
 				</div>
 				<button className='outline-none py-[12px] px-[20px] bg-[#DAF1E4] rounded-[15px] text-[18px] text-[#008037] lg:hidden'>
 					Contact Us
@@ -49,25 +68,25 @@ const Navbar = () => {
 						alt='hamburger'
 					/>
 				</div>
-				{navDisplay && (
-					<div className='bg-gray-600/30 backdrop-blur-sm w-full h-[100%] fixed left-0 top-[72px] z-[100]'>
-						<div className='mt-[20px] mx-auto grid text-[16px] text-center gap-4 w-[90%] bg-white rounded-md font-medium py-8'>
-							<span className='select-none opacity-70 hover:opacity-90'>
-								<button href='#'>Home</button>
-							</span>
-							<span className='select-none opacity-70 hover:opacity-90'>
-								<button href='#'>Businesses</button>
-							</span>
-							<span className='select-none opacity-70 hover:opacity-90'>
-								<button href='#'>About</button>
-							</span>
-							<span className='select-none opacity-70 hover:opacity-90'>
-								<button href='#'>Careers</button>
-							</span>
-						</div>
-					</div>
-				)}
 			</div>
+			{navDisplay && (
+				<div className='bg-gray-600/30 backdrop-blur-sm w-full h-[100%] fixed left-0 top-[72px] z-[100]'>
+					<div className='mt-[20px] mx-auto grid text-[16px] text-center gap-4 w-[90%] bg-white rounded-md font-medium py-8'>
+						<span className='select-none opacity-70 hover:opacity-90'>
+							<button href='#'>Home</button>
+						</span>
+						<span className='select-none opacity-70 hover:opacity-90'>
+							<button href='#'>Businesses</button>
+						</span>
+						<span className='select-none opacity-70 hover:opacity-90'>
+							<button href='#'>About</button>
+						</span>
+						<span className='select-none opacity-70 hover:opacity-90'>
+							<button href='#'>Careers</button>
+						</span>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 };
